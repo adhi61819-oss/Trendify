be7b563db93da5bf9a6636659e2ee9cdb04c31b8
adhi61819-oss <adhi61819@gmail.com>
2026-02-21 15:17:23 +0530

    Create src/App.js file

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Trendify from './components/Trendify';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Trendify />
+    </div>
+  );
+}
+
+export default App;
